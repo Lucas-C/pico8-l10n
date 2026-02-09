3caf2c4f698ef7b46a91d837f724d25c091a2f00
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2026-02-09 09:07:44 +0100

    Translation for Sundered Hope

--- tests/p8_spec.lua
+++ tests/p8_spec.lua
@@ -68,6 +68,16 @@ end
 ]]
       assert.same({ ["picohot_v2"] = { 90 } }, p8.extract_strings_p8_content(code))
     end)
+    it("should correctly extract strings like __x__", function()
+      local code = [[
+pico-8 cartridge // http://www.pico-8.com
+version 43
+__lua__
+print("__x__")
+print("OK")
+]]
+      assert.same({ ["__x__"] = { 69 }, ["OK"] = { 84 } }, p8.extract_strings_p8_content(code))
+    end)
   end)
   describe(".subst_l10n_strings()", function()
     it("should successfully substitute localized strings in .p8 file content", function()
